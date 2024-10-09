cd4f0db2f9d983ffa4c8b5d6f21508bb89828a7c
Lucio <lu30ya31@hotmail.com>
2024-10-09 14:54:50 -0700

    Character Blueprint Setup

---
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -0,0 +1,24 @@
+// Lucio Martinez.
+
+
+#include "AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),"WeaponHandSocket");
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
