efa2bd65f08dea8ce6616da0634f8ea7270be78b
Lucio <lu30ya31@hotmail.com>
2024-11-29 11:23:20 -0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Lucio Martinez.
+
+
+#include "AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
