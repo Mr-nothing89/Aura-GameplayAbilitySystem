a73e5c08459e552db50f578903b611cb41368dd6
Lucio <lu30ya31@hotmail.com>
2024-12-05 13:41:37 -0800

    Attribute Info Delegate

--- Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -3,6 +3,10 @@
 
 #include "AttributeMenuWidgetController.h"
 
+#include "Aura/AuraGameplayTags.h"
+#include "Aura/AbilitySystem/AuraAttributeSet.h"
+#include "Aura/AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 	
@@ -10,5 +14,11 @@ void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
-	
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	Info.AttributeValue = AS->GetStrength();
+
+	AttributeInfoDelegate.Broadcast(Info);
 }
