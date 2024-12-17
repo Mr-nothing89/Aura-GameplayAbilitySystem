20cecc70bd9f16db80828ca24de597f7e8c31ecc
Lucio <lu30ya31@hotmail.com>
2024-12-16 16:20:33 -0800

    Projectile Collision Channel

--- Source/Aura/Character/AuraCharacterBase.cpp
+++ Source/Aura/Character/AuraCharacterBase.cpp
@@ -4,6 +4,7 @@
 #include "AuraCharacterBase.h"
 
 #include "AbilitySystemComponent.h"
+#include "Aura/Aura.h"
 #include "Aura/AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Components/CapsuleComponent.h"
 
@@ -15,6 +16,8 @@ AAuraCharacterBase::AAuraCharacterBase()
 
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera,ECR_Ignore);
+	GetMesh()->SetGenerateOverlapEvents(true);
+	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile,ECR_Overlap);
 
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
 	Weapon->SetupAttachment(GetMesh(),"WeaponHandSocket");
