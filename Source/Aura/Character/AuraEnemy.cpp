99e6df5f18f3d729e8b3e444bc87e7383ee4bf60
Lucio <lu30ya31@hotmail.com>
2025-02-12 16:42:06 -0800

    AI and Effect Actors

--- Source/Aura/Character/AuraEnemy.cpp
+++ Source/Aura/Character/AuraEnemy.cpp
@@ -24,6 +24,11 @@ AAuraEnemy::AAuraEnemy()
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+
 	HealthBar = CreateDefaultSubobject<UWidgetComponent>("HealthBar");
 	HealthBar->SetupAttachment(GetRootComponent());
 }
