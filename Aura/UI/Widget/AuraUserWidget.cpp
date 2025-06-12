eb4fd7e9504d8eee31f6421d20d21c4a4d13c3c7
Lucio Martinez <lu30ya31@hotmail.com>
2025-06-11 17:58:50 -0700

    Aura User Widget and Widget Controller

---
+++ Aura/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Gameplay Ability System.
+
+
+#include "AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Aura/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,35 @@
+// Gameplay Ability System.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+class UAttributeSet;
+class UAbilitySystemComponent;
+/**
+ * Intermediary class.
+ *
+ */
+UCLASS()
+class AURA_API UAuraWidgetController : public UObject
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly,Category="Widget Controller")
+	TObjectPtr<APlayerController> PlayerController;
+
+	UPROPERTY(BlueprintReadOnly,Category="Widget Controller")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly,Category="Widget Controller")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(BlueprintReadOnly,Category="Widget Controller")
+	TObjectPtr<UAttributeSet> AttributeSet;
+	
+	
+};
