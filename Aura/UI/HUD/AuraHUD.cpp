3d418c5ee700c5681532dc37c1421813d3efbed8
Lucio Martinez <lu30ya31@hotmail.com>
2025-06-11 19:57:12 -0700

    Aura HUD

---
+++ Aura/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,13 @@
+// Gameplay Ability System.
+
+
+#include "AuraHUD.h"
+#include "Aura/UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(),OverlayWidgetClass);
+	Widget->AddToViewport();
+}
