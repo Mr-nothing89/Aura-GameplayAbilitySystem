1b6917551426bdf3ced65561e615327d40af29f9
Lucio Martinez <lu30ya31@hotmail.com>
2025-09-09 12:10:20 -0700

    Enemy Death

--- Aura/AbilitySystem/AuraAttributeSet.cpp
+++ Aura/AbilitySystem/AuraAttributeSet.cpp
@@ -7,6 +7,7 @@
 #include "Net/UnrealNetwork.h"
 #include "GameplayEffectExtension.h"
 #include "Aura/AuraGameplayTags.h"
+#include "Aura/Interaction/CombatInterface.h"
 #include "GameFramework/Character.h"
 
 
@@ -112,7 +113,15 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallba
 			SetHealth(FMath::Clamp(NewHealth,0.f,GetMaxHealth()));
 
 			const bool bFatal = NewHealth <= 0.f;
-			if (!bFatal)
+			if (bFatal)
+			{
+				ICombatInterface* CombatInterface = Cast<ICombatInterface>(Props.TargetAvatarActor);
+				if (CombatInterface)
+				{
+					CombatInterface->Die();
+				}
+			}
+			else
 			{
 				FGameplayTagContainer TagContainer;
 				TagContainer.AddTag(FAuraGameplayTags::Get().Effects_HitReact);
